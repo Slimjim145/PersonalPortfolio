0aa33b35c7cdf656e5c898f52d84c03606c0b482
Slimjim <fang241999@gmail.com>
2020-07-20 19:47:48 -0400

    github pages setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+
 function Header(){
     return (
         <div className="jumbotron">
             <h1>Fabian Guerrier</h1>
             <p>
-                <Link to="/">
-                Home
-                </Link>
+                <Link to="/">Home</Link>
+                <Link to="/projects">Projects</Link>
                 <a href="https://github.com/Slimjim145">
                     GitHub 
                 </a>
